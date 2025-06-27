f8b1ee24d09452d61d81c6e497b4df17395ef528
mohammed0982 <simmoha08@gmail.com>
2025-06-27 14:14:28 +0000

    Add files via upload

---
+++ my/index.js
@@ -0,0 +1,31 @@
+const { Client, LocalAuth, MessageMedia } = 
+require('whatsapp-web.js'); const qrcode = 
+require('qrcode-terminal'); const path = require('path'); const 
+client = new Client({
+    authStrategy: new LocalAuth(), puppeteer: { headless: true, 
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
+client.on('qr', qr => { qrcode.generate(qr, { small: true });
+});
+client.on('ready', () => { console.log('Client is ready!');
+});
+client.on('message', async message => { if (message.body === 
+    '!ping') {
+        await message.reply('pong');
+    }
+    
+    const receivedText = message.body.toLowerCase(); if 
+    (receivedText === 'mong-tv') {
+        try { const apkPath = path.join(__dirname, 'files', 
+            'Mong-tv.apk'); const media = await 
+            MessageMedia.fromFilePath(apkPath); await 
+            message.reply(' جارٍ إرسال تطبيق Mong-tv إليك...'); 
+            await client.sendMessage(message.from, media);
+        } catch (error) {
+            console.error('Error sending file:', error); await 
+            message.reply('❌ حدث خطأ أثنا إرسال الل');
+        }
+    }
+});
+client.initialize();
